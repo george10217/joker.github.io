5c47a7eb27f87c3ea5d7f721072df6adcd4d89c7
george10217 <66462839+george10217@users.noreply.github.com>
2020-06-14 14:30:50 +0930

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,37 +1,100 @@
 
-
-function setShirt() {       // Checks & outputs an alert to display the Pizza Make to either 'Classic', 'Legend' or 'Signature'
-    if (document.Options.shirtSelected[0].checked == true) {
-    }
-    else if (document.Options.shirtSelected[1].checked == true) {
-        }
-else if (document.Options.shirtSelected[2].checked == true) {
-        }
-
-}
-
-function getOptions(shirtSelected) {
-    var list;
-    var listValue;
-   
-        list = ["Standard ship","3 day","Overnight"];  // Creates an array called list populating it with pizza type
-        listValue = [10,60,90];  // Creates an array called listValue populating it with pizza prices
-    
-    for (var i = 0; i < document.Options.shirtSelected.length; i++) {         //Clear the dropdown menu of old dropdown list
-		document.Options.dList.options[i] = null;
-	}
-	for (var i = 0; i < list.length; i++) {                                     //Re-populates the dropdown menu with new dropdown list
-		document.Options.dList.options[i] = new Option(list[i],listValue[i],0,0);
-	
-	}
-}
-
-
-
-function myDisplay(i) {
-    document.getElementById("listValue").innerHTML = "$ " + i;
-}
-
+//*This allows the table to appear and the map to disappear on click through the application of jquery*//
 function appear(){
-           $('#table').fadeIn(400);
+           $('#table').fadeIn(1500);
+           $('.info').fadeOut(500);
 }
+
+function appear1(){
+           $('#check').fadeIn(400);
+}
+
+function ething(){
+           appear1();
+           calculateTotal();
+           totaloptions();
+           totalshippingcost();
+           
+}
+
+
+ var shirt_prices = new Array();
+ shirt_prices["Shirt1"]=100;
+ shirt_prices["Shirt2"]=150;
+ shirt_prices["Shirt3"]=180;
+ shirt_prices["Noshirt"]=0;
+ 
+ function getShirtprice(){  
+    var Shirtprice=0;
+    var theForm = document.forms["Options"];
+    var selectedshirt = theForm.elements["shirtSelected"];
+    for(var i = 0; i < selectedshirt.length; i++)
+    {
+        if(selectedshirt[i].checked)
+        {
+            Shirtprice = shirt_prices[selectedshirt[i].value];
+            break;
+        }
+    }
+    return Shirtprice;
+}
+
+var pantprices = new Array();
+ pantprices["Pant1"]=200;
+ pantprices["Pant2"]=250;
+ pantprices["Pant3"]=290;
+ pantprices["Nopant"]=0;
+
+function getPantprice(){  
+    var pantPrice=0;
+    var theForm1 = document.forms["Options"];
+    var selectedP = theForm1.elements["pantSelected"];
+    for(var x = 0; x < selectedP.length; x++)
+    {
+        if(selectedP[x].checked)
+        {
+            pantPrice = pantprices[selectedP[x].value];
+            break;
+        }
+    }
+    return pantPrice;
+}
+
+
+ var shipping_prices= new Array();
+ shipping_prices["None"]=0;
+ shipping_prices["Stand"]=30;
+ shipping_prices["threeday"]=50;
+ shipping_prices["Onight"]=90;
+
+
+
+function getshipprice(){
+    var shipping=0;
+    var theForm = document.forms["Options"];
+     var selectedshipping = theForm.elements["dList"];
+
+    shipping = shipping_prices[selectedshipping.value];
+
+    return shipping;
+
+}
+
+
+function calculateTotal(){
+    var adminfee = 40;
+    var total = adminfee + getShirtprice() + getPantprice() + getshipprice();
+    
+
+    var tnow = document.getElementById('totalPrice');
+    tnow.innerHTML = total;
+}
+
+function totaloptions(){
+           var totatclothes = getShirtprice() + getPantprice();
+           
+           var options = document.getElementById('totaloption');
+           options.innerHTML = totatclothes;
+}
+
+
